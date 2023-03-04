8ac6348ece68e48b8ed04603e3a056cd9c991967
TarouShota <adilov_02@mail.ru>
2023-03-04 18:06:49 +0000

    send requests to gpt3, delete weather API

--- go.mod
+++ go.mod
@@ -3,16 +3,23 @@ module github.com/mymmrac
 go 1.19
 
 require (
+	github.com/joho/godotenv v1.5.1
+	github.com/mymmrac/telego v0.16.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/canthefason/go-watcher v0.2.4 // indirect
 	github.com/fasthttp/router v1.4.11 // indirect
+	github.com/fatih/color v1.14.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
-	github.com/mymmrac/telego v0.16.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
 	github.com/spf13/cobra v1.5.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.39.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,58 +1,98 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"github.com/joho/godotenv"
 	"os"
 	// "telego/telego"
 	// "goBot/go"
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
-    tu "github.com/mymmrac/telego/telegoutil"
-	"github.com/joho/godotenv"
+	tu "github.com/mymmrac/telego/telegoutil"
 
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+func sendGptRequest(message string) string {
+	apiKey := os.Getenv("AITOKEN")
+	url := "https://api.openai.com/v1/chat/completions"
 
+	data := map[string]interface{}{
+		"model": "gpt-3.5-turbo",
+		"messages": []map[string]string{{
+			"role":    "user",
+			"content": message,
+		}},
+	}
 
-func getHolidays() string{
-    resp, err := http.Get("https://calendarific.com/api/v2/holidays?&api_key="+os.Getenv("CALENDAR_API")+"&country=CZ&year=2022&month=10")
-    if err != nil {
-       log.Fatalln(err)
-    }
- //We Read the response body on the line below.
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-       log.Fatalln(err)
-    }
- //Convert the body to type string
-    
-    sb := string(body)
-    return sb
+	payload, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
 
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	var result map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		panic(err)
+	}
+
+	choices, ok := result["choices"].([]interface{})
+	if !ok {
+		panic("Invalid response format")
+	}
+
+	if len(choices) == 0 {
+		return ""
+	}
+
+	messageMap, ok := choices[0].(map[string]interface{})["message"].(map[string]interface{})
+	if !ok {
+		panic("Invalid response format")
+	}
+
+	return messageMap["content"].(string)
 }
-
 
 func main() {
 	err := godotenv.Load(".env")
-	// os.Setenv("TOKEN","1")
-    botToken := os.Getenv("TOKEN")
-    bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    // botUser, err := bot.GetMe()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	// os.Setenv("TOKEN","")
+	log.Println(os.Getenv("TOKEN"))
 
+	botToken := os.Getenv("TOKEN")
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    	// Get updates channel
+	// botUser, err := bot.GetMe()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Get updates channel
 	updates, _ := bot.UpdatesViaLongPulling(nil)
 
 	// Create bot handler and specify from where to get updates
@@ -64,31 +104,31 @@ func main() {
 	// Stop getting updates
 	defer bot.StopLongPulling()
 
-    //request
-   
-
+	//request
 
 	// Register new handler with match on command `/start`
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
 		// Send message
 		_, _ = bot.SendMessage(tu.Message(
 			tu.ID(update.Message.Chat.ID),
-			fmt.Sprintf("Hello %s!", getHolidays()),
+			fmt.Sprintf("Hello! How can I assist you today?"),
 		))
 	}, th.CommandEqual("start"))
 
-	// Register new handler with match on any command
-	// Handlers will match only once and in order of registration, 
-	// so this handler will be called on any command except `/start` command
-	bh.Handle(func(bot *telego.Bot, update telego.Update) {
-		// Send message
+	bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
+		// Get chat ID from the message
+		chatID := tu.ID(message.Chat.ID)
+		log.Println(message.Text)
+
+		//now make request to the open ai api
+
 		_, _ = bot.SendMessage(tu.Message(
-			tu.ID(update.Message.Chat.ID),
-			"Unknown command, use /start",
+			chatID,
+			sendGptRequest(message.Text),
 		))
-	}, th.AnyCommand())
+
+	})
 
 	// Start handling updates
-    bh.Start()
+	bh.Start()
 }
-
